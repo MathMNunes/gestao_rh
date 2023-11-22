2ef0b73507e88f1dc6d6174b7d0680024e63841b
MathMNunes <matheusmarques9@hotmail.com>
2023-11-22 10:05:25 -0300

    feat: create model employee

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    #APPS
     'apps.company',
+    'apps.funcionarios',
 ]
 
 MIDDLEWARE = [
